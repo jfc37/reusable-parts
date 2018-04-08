0e88820f73f86a074c6d7539411c5746cc91d6ac
JoeC <placid.joe@gmail.com>
2018-04-08 17:45:55 +1200

    Revert "Code tidy up"
    This reverts commit 33dbacd586cdb486278b48d044ab1c3984776e90.

--- libs/login-page/src/components/login-page/login-page.component.ts
+++ libs/login-page/src/components/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { LOGIN_PAGE_CONFIG, LoginPageConfig } from '@reusable-parts/login-page/src/login-page.config';
+import { LOGIN_PAGE_CONFIG } from '@reusable-parts/login-page/src/login-page.config';
 
 @Component({
   selector: 'jfc-login-page',
@@ -7,6 +7,8 @@ import { LOGIN_PAGE_CONFIG, LoginPageConfig } from '@reusable-parts/login-page/s
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
-  constructor(@Inject(LOGIN_PAGE_CONFIG) public config: LoginPageConfig) { }
+  constructor(@Inject(LOGIN_PAGE_CONFIG) public config: LoginPageConfig) {
+    console.error('xxx CONFIG', config);
+   }
 
 }
