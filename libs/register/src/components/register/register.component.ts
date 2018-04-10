4ff5fa13c0f8727ec5af654057560a3a9da51379
JoeC <placid.joe@gmail.com>
2018-04-10 19:55:21 +1200

    Taken html and styles from fuse

--- libs/register/src/components/register/register.component.ts
+++ libs/register/src/components/register/register.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { fuseAnimations } from '@reusable-parts/@fuse/animations';
 
 @Component({
   selector: 'jfc-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  animations: fuseAnimations
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  public ngOnInit() {
+    this.registerForm = new FormBuilder().group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      passwordConfirm: ['', [Validators.required, confirmPassword]]
+    });
   }
 
+  public displayError(field: string): boolean {
+    const control = this.registerForm.get(field);
+    return control && control.invalid && control.touched;
+  }
+}
+
+function confirmPassword(control: AbstractControl) {
+  if (!control.parent || !control) {
+    return;
+  }
+
+  const password = control.parent.get('password');
+  const passwordConfirm = control.parent.get('passwordConfirm');
+
+  if (!password || !passwordConfirm) {
+    return;
+  }
+
+  if (passwordConfirm.value === '') {
+    return;
+  }
+
+  if (password.value !== passwordConfirm.value) {
+    return {
+      passwordsNotMatch: true
+    };
+  }
 }
--- libs/register/src/register.module.ts
+++ libs/register/src/register.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './components/register/register.component';
+import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FuseSharedModule } from '@reusable-parts/@fuse';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+
+    FuseSharedModule
+  ],
   declarations: [RegisterComponent],
   exports: [RegisterComponent],
 })
