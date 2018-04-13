7dff744b3a2c4eaddb68e2b315250ba34d682b43
JoeC <placid.joe@gmail.com>
2018-04-14 09:39:16 +1200

    Redirect to hardcoded login route when not authenticated

--- libs/guards/src/authenticated/authenticated.guard.ts
+++ libs/guards/src/authenticated/authenticated.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { FirebaseAuthService } from '@reusable-parts/guards/src/services/firebase-auth.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
-  constructor(private authService: FirebaseAuthService) { }
+  constructor(
+    private authService: FirebaseAuthService,
+    private router: Router,
+  ) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> {
-    return this.authService.isAuthenticated();
+    return this.authService.isAuthenticated().pipe(
+      tap(isAuthenticated => {
+        if (!isAuthenticated) {
+          this.router.navigate(['login']);
+        }
+      })
+    );
   }
 }
--- libs/guards/src/services/firebase-auth.service.ts
+++ libs/guards/src/services/firebase-auth.service.ts
@@ -14,5 +14,4 @@ export class FirebaseAuthService {
       map(user => Boolean(user)),
     );
   }
-
 }
