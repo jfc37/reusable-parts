36026ee62a9f0a5f89ce46dbff6bda1ca47c3c5b
JoeC <placid.joe@gmail.com>
2018-04-15 11:11:32 +1200

    Tidy up of side nav

--- libs/side-nav/src/components/side-nav/side-nav.component.ts
+++ libs/side-nav/src/components/side-nav/side-nav.component.ts
@@ -1,26 +1,33 @@
-import { Component, Input, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, Input, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
 import { FuseConfigService } from '@reusable-parts/@fuse/services/config.service';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'jfc-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss'],
   encapsulation: ViewEncapsulation.None
-
 })
-export class SideNavComponent implements OnInit
-{
-  fuseSettings: any;
-  constructor(
-    private fuseConfig: FuseConfigService,
-  ) {}
+export class SideNavComponent implements OnInit, OnDestroy {
+  public folded: boolean;
+  public sideBarClasses: any;
+
+  private onDestroy$ = new ReplaySubject();
+
+  constructor(private fuseConfig: FuseConfigService) { }
 
   public ngOnInit(): void {
     this.fuseConfig.onConfigChanged
-        .subscribe(
-            (newSettings) => {
-                this.fuseSettings = newSettings;
-            }
-        );
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe(newSettings => {
+        this.folded = newSettings.layout.navigationFolded;
+        this.sideBarClasses = newSettings.colorClasses.navbar;
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.onDestroy$.next(null);
+    this.onDestroy$.complete();
   }
 }
