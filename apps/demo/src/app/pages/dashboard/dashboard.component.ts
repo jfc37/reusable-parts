1baac0a53b84fb6041dfc8217f293dea876f00b7
JoeC <placid.joe@gmail.com>
2018-04-22 15:52:01 +1200

    Removed demo project

--- apps/demo/src/app/pages/dashboard/dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'jfc-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
-})
-export class DashboardComponent {
-}
