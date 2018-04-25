7498cc4797a4dad57ff6dbe90796a1038d80929d
JoeC <placid.joe@gmail.com>
2018-04-25 21:40:45 +1200

    Alert boxes

--- libs/common-ui-components/src/common-ui-components.module.ts
+++ libs/common-ui-components/src/common-ui-components.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './components/loader/loader.component';
-import { MatProgressSpinnerModule } from '@angular/material';
+import { MatProgressSpinnerModule, MatIconModule } from '@angular/material';
 import { FuseSharedModule } from '@reusable-parts/@fuse';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
-  imports: [CommonModule, MatProgressSpinnerModule, FuseSharedModule],
-  declarations: [LoaderComponent],
-  exports: [LoaderComponent],
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    FuseSharedModule,
+  ],
+  declarations: [LoaderComponent, AlertComponent],
+  exports: [LoaderComponent, AlertComponent],
 })
 export class CommonUiComponentsModule {}
---
+++ libs/common-ui-components/src/components/alert/alert.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'jfc-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss'],
+})
+export class AlertComponent implements OnChanges {
+  @Input() public type: AlertType;
+
+  public colourClass: string;
+  public icon: string;
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    switch (this.type) {
+      case AlertType.error: {
+        this.colourClass = 'mat-warn-100-bg';
+        this.icon = 'error_outline';
+        break;
+      }
+
+      case AlertType.warning: {
+        this.colourClass = 'mat-orange-100-bg';
+        this.icon = 'warning';
+        break;
+      }
+
+      case AlertType.info: {
+        this.colourClass = 'mat-accent-100-bg';
+        this.icon = 'info_outline';
+        break;
+      }
+
+      case AlertType.success: {
+        this.colourClass = 'mat-green-100-bg';
+        this.icon = 'check';
+        break;
+      }
+    }
+  }
+}
+
+export enum AlertType {
+  error = 'error',
+  warning = 'warning',
+  info = 'info',
+  success = 'success',
+}
