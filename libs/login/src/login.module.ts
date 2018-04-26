8ad77b05a56bfcfd498dabb08631e32870d7c745
JoeC <placid.joe@gmail.com>
2018-04-26 18:17:14 +1200

    Error and success alerts

--- libs/login/src/login.module.ts
+++ libs/login/src/login.module.ts
@@ -11,6 +11,7 @@ import { RouterModule } from '@angular/router';
 import { FuseSharedModule } from '@reusable-parts/@fuse';
 import { LoginComponent } from './components/login/login.component';
 import { WelcomeModule } from '@reusable-parts/welcome';
+import { CommonUiComponentsModule } from '@reusable-parts/common-ui-components';
 
 @NgModule({
   imports: [
@@ -20,6 +21,7 @@ import { WelcomeModule } from '@reusable-parts/welcome';
     RouterModule,
 
     WelcomeModule,
+    CommonUiComponentsModule,
 
     // angular material components
     MatButtonModule,
