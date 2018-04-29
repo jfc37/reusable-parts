23303ca56efded56e3d8090b84202cec67ffffce
JoeC <placid.joe@gmail.com>
2018-04-29 13:51:00 +1200

    Fixing linting

--- apps/fullswing/src/app/admin/view-blocks-page/view-blocks-page/view-blocks-page.component.ts
+++ apps/fullswing/src/app/admin/view-blocks-page/view-blocks-page/view-blocks-page.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'jfc-view-blocks-page',
   templateUrl: './view-blocks-page.component.html',
-  styleUrls: ['./view-blocks-page.component.scss']
+  styleUrls: ['./view-blocks-page.component.scss'],
 })
 export class ViewBlocksPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
