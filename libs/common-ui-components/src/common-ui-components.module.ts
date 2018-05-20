f2deec87f4d78fa7f20b64a16bb3e587fc74476e
JoeC <placid.joe@gmail.com>
2018-05-20 16:30:59 +1200

    Elevate on hover

--- libs/common-ui-components/src/common-ui-components.module.ts
+++ libs/common-ui-components/src/common-ui-components.module.ts
@@ -4,10 +4,11 @@ import { LoaderComponent } from './components/loader/loader.component';
 import { MatProgressSpinnerModule, MatIconModule } from '@angular/material';
 import { FuseSharedModule } from '@reusable-parts/@fuse';
 import { AlertComponent } from './components/alert/alert.component';
+import { ElevateOnHoverDirective } from './directives/elevate-on-hover.directive';
 
 @NgModule({
   imports: [CommonModule, MatProgressSpinnerModule, MatIconModule, FuseSharedModule],
-  declarations: [LoaderComponent, AlertComponent],
-  exports: [LoaderComponent, AlertComponent],
+  declarations: [LoaderComponent, AlertComponent, ElevateOnHoverDirective],
+  exports: [LoaderComponent, AlertComponent, ElevateOnHoverDirective],
 })
 export class CommonUiComponentsModule {}
