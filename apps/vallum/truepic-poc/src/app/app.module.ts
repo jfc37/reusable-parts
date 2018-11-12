41676ce955047b131bbe9b2a042a5745f887c8c3
JoeC <placid.joe@gmail.com>
2018-11-12 18:10:06 +1300

    Home component

--- apps/vallum/truepic-poc/src/app/app.module.ts
+++ apps/vallum/truepic-poc/src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { CommonUiComponentsModule } from '@reusable-parts/common-ui-components/src';
+import { HomeComponent } from './home/home.component';
+import { MatButtonModule } from '@angular/material';
+import { FuseSharedModule, FuseModule } from '@reusable-parts/@fuse';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     CommonUiComponentsModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(
+      [{ path: 'home', component: HomeComponent }, { path: '', pathMatch: 'full', redirectTo: 'home' }],
+      { initialNavigation: 'enabled' },
+    ),
+    MatButtonModule,
+
+    FuseModule.forRoot({}),
+    FuseSharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
