a7e06233ba852cc5893c79891bdd9877087b0276
JoeC <placid.joe@gmail.com>
2018-11-12 17:46:15 +1300

    Added simple layout componet

--- apps/vallum/truepic-poc/src/app/app.module.ts
+++ apps/vallum/truepic-poc/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
+import { CommonUiComponentsModule } from '@reusable-parts/common-ui-components/src';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot(), RouterModule.forRoot([], { initialNavigation: 'enabled' })],
+  imports: [
+    BrowserModule,
+    CommonUiComponentsModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ libs/common-ui-components/src/lib/components/simple-layout/simple-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { fuseAnimations } from '@reusable-parts/@fuse/animations';
+
+@Component({
+  selector: 'jfc-simple-layout',
+  templateUrl: './simple-layout.component.html',
+  styleUrls: ['./simple-layout.component.scss'],
+  animations: fuseAnimations,
+})
+export class SimpleLayoutComponent {}
