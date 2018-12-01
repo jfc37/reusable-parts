a701562302a8265c8d4dec617f5042f2e83fc28a
JoeC <placid.joe@gmail.com>
2018-12-01 14:24:42 +1300

    Documenting how to set up POC

--- libs/serverless-snippets/src/lib/auth0/create-user-in-copper.js
+++ libs/serverless-snippets/src/lib/auth0/create-user-in-copper.js
@@ -52,7 +52,7 @@ function getCopperUser(user) {
 function getHeaders() {
   return {
     ['X-PW-AccessToken']: '3150a263724e1acaf24b651f7b68c661',
-    ['X-PW-Application']: 'developer_api',
     ['X-PW-UserEmail']: 'joe.chapman3737@gmail.com',
+    ['X-PW-Application']: 'developer_api',
   };
 }
