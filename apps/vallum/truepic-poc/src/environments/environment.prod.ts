d47823795802adc71bf2935d2cad8a8d666d19df
JoeC <placid.joe@gmail.com>
2019-01-23 18:06:20 +1300

    correcting prod endpoint

--- apps/vallum/truepic-poc/src/environments/environment.prod.ts
+++ apps/vallum/truepic-poc/src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
   production: true,
-  domain: 'http://localhost:4200/',
+  domain: 'https://vallum-truepic.firebaseapp.com/',
 
   auth0: {
     clientId: '9hW9Hc2RJqT2RL1QWyI-IUvuTeayrYS2',
     domain: 'jfc37.au.auth0.com',
-    redirectUri: 'http://localhost:4200/callback',
+    redirectUri: 'https://vallum-truepic.firebaseapp.com/callback',
     unauthorisedRoute: 'welcome',
   },
 
