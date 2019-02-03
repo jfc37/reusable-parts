e92d8f0d338f6b35d3664b3079f51658b3639bc5
JoeC <placid.joe@gmail.com>
2019-02-04 11:10:46 +1300

    chat components

--- libs/@fuse/index.ts
+++ libs/@fuse/index.ts
@@ -1,2 +1,5 @@
 export { FuseSharedModule } from './shared.module';
+export { FuseUtils } from './utils';
 export { FuseModule } from './fuse.module';
+export * from './directives/directives';
+export { FusePerfectScrollbarDirective } from './directives/fuse-perfect-scrollbar/fuse-perfect-scrollbar.directive';
--- libs/chat-components/src/lib/chat-components.module.ts
+++ libs/chat-components/src/lib/chat-components.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChatComponent } from './chat.component';
+import { ChatViewComponent } from './chat-view/chat-view.component';
+import { ChatStartComponent } from './chat-start/chat-start.component';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatRadioModule,
+  MatSidenavModule,
+  MatToolbarModule,
+} from '@angular/material';
+import { FuseSharedModule } from '@reusable-parts/@fuse';
+import { ChatService } from './chat.service';
 
 @NgModule({
-  imports: [CommonModule],
+  declarations: [ChatComponent, ChatViewComponent, ChatStartComponent],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatRadioModule,
+    MatSidenavModule,
+    MatToolbarModule,
+
+    FuseSharedModule,
+  ],
+  providers: [ChatService],
 })
 export class ChatComponentsModule {}
