f11474f940c7d38d4406969c5e524d210b48ef4b
JoeC <placid.joe@gmail.com>
2019-02-17 12:26:53 +1300

    added firebase config to prod environment

--- apps/demos/chat-app/src/environments/environment.prod.ts
+++ apps/demos/chat-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyCkrchO-F45UQTjpo1IoFheJYRKqLyNTb0',
+    authDomain: 'hono-chat.firebaseapp.com',
+    databaseURL: 'https://hono-chat.firebaseio.com',
+    projectId: 'hono-chat',
+    storageBucket: 'hono-chat.appspot.com',
+    messagingSenderId: '1096356841695',
+  },
 };
