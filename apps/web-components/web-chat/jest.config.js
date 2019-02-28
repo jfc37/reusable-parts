a925c09f241adf7db7e237a402e5299f9d390ac7
JoeC <placid.joe@gmail.com>
2019-02-28 14:25:00 +1300

    adding new web chat app

---
+++ apps/web-components/web-chat/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'web-components-web-chat',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/web-components/web-chat/',
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js',
+  ],
+};
