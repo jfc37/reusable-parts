055af0d41f3901d1c51e3fdac09dae8ce0822781
JoeC <placid.joe@gmail.com>
2019-03-06 21:36:08 +1300

    code tidy up

--- apps/web-components/web-chat/src/app/app.module.ts
+++ apps/web-components/web-chat/src/app/app.module.ts
@@ -9,14 +9,10 @@ import { ChatComponentsModule, ChatComponent } from '@reusable-parts/chat-compon
   entryComponents: [ChatComponent],
 })
 export class AppModule {
-  constructor(private injector: Injector) {
+  constructor(injector: Injector) {
     const el = createCustomElement(ChatComponent, { injector });
 
     customElements.define('web-chat', el);
   }
-  // constructor(private injector: Injector) {}
-  ngDoBootstrap() {
-    // const el = createCustomElement(AppComponent, { injector: this.injector });
-    // customElements.define('web-chat', el);
-  }
+  ngDoBootstrap() {}
 }
