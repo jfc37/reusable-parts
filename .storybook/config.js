78696f8e8bf9a95c95b1610cb8af80991ee552f7
JoeC <placid.joe@gmail.com>
2019-03-08 22:24:59 +1300

    storybook source

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { configure } from '@storybook/angular';
 
 function loadStories() {
-  require('../libs/storybook/src/index.ts');
+  // load all from  /src/stories/*.stories.ts files
+  const req = require.context('../libs/storybook', true, /\.stories\.ts$/);
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: [/\.stories\.tsx?$/, /index\.ts$/],
+        loaders: [
+          {
+            loader: require.resolve('@storybook/addon-storysource/loader'),
+            options: {
+              parser: 'typescript',
+            },
+          },
+        ],
+        include: [path.resolve(__dirname, '../libs/storybook')],
+        enforce: 'pre',
+      },
+    ],
+  },
+};
