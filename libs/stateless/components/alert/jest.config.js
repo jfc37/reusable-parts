7d6710fa1046b6adf0acc607eee49e6e47687dc7
JoeC <placid.joe@gmail.com>
2019-03-16 15:53:04 +1300

    correcting config file paths

--- libs/stateless/components/alert/jest.config.js
+++ libs/stateless/components/alert/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'stateless-alert',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/libs/stateless/components/alert',
+  preset: '../../../../jest.config.js',
+  coverageDirectory: '../../../../coverage/libs/stateless/components/alert',
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js',
--- libs/stateless/components/loader/jest.config.js
+++ libs/stateless/components/loader/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'stateless-loader',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/libs/stateless/components/loader',
+  preset: '../../../../jest.config.js',
+  coverageDirectory: '../../../../coverage/libs/stateless/components/loader',
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js',
