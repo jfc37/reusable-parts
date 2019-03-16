c288838335ded7da4463061f9a617a512f58615f
JoeC <placid.joe@gmail.com>
2019-03-16 15:35:59 +1300

    deleted stateless welcome

--- libs/stateless/welcome/index.ts
+++
@@ -1,2 +0,0 @@
-export { StatelessWelcomeModule } from './src/lib/stateless-welcome.module';
-export { StatelessWelcomeComponent } from './src/lib/welcome/welcome.component';
--- libs/stateless/welcome/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'stateless-welcome',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/libs/stateless/welcome',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js',
-  ],
-};
