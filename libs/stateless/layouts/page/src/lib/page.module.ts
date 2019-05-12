481be7a082a880b52201add431f23e5c6754df52
JoeC <placid.joe@gmail.com>
2019-05-12 21:23:04 +1200

    remove padding for mobile

--- libs/stateless/layouts/page/src/lib/page.module.ts
+++ libs/stateless/layouts/page/src/lib/page.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderModule } from '@reusable-parts/stateless/layouts/header';
 import { HeroTextModule } from '@reusable-parts/stateless/components/hero-text';
 import { PageComponent } from './page.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports: [CommonModule, HeaderModule, HeroTextModule],
+  imports: [CommonModule, FlexLayoutModule, HeaderModule, HeroTextModule],
   declarations: [PageComponent],
   exports: [PageComponent],
 })
